b9a8c276ac5884fbcf4ec732867b9122851f3065
GitMurickumthanathu <murickumthanathu@gmail.com>
2025-07-29 10:31:05 +0300

    Add files via upload

--- gradlew.bat
+++ gradlew.bat
@@ -1,2 +1,19 @@
-@echo off
-java -jar gradle\wrapper\gradle-wrapper.jar %*
+@if "%DEBUG%" == "" @echo off
+@rem ##########################################################################
+@rem
+@rem  Gradle startup script for Windows
+@rem
+@rem ##########################################################################
+
+setlocal
+
+set DIR=%~dp0
+set GRADLE_WRAPPER_JAR=%DIR%gradle\wrapper\gradle-wrapper.jar
+set GRADLE_WRAPPER_PROPERTIES=%DIR%gradle\wrapper\gradle-wrapper.properties
+
+if exist "%GRADLE_WRAPPER_JAR%" (
+    java -classpath "%GRADLE_WRAPPER_JAR%" org.gradle.wrapper.GradleWrapperMain %*
+) else (
+    echo "Could not find gradle-wrapper.jar"
+    exit /b 1
+)
